2285b703991f3e3722240cf3c8ecc78c10efc316
Taliesin Millhouse <taliesin.millhouse@maxkelsen.com>
2022-02-12 16:16:42 +1100

    feat: added tests

--- internal/hello-world/run_test.go
+++ internal/hello-world/run_test.go
@@ -1,23 +1,31 @@
 package helloworld_test
 
 import (
+	"bytes"
+	"context"
 	"testing"
+
+	"github.com/sethvargo/go-githubactions"
+	"github.com/stretchr/testify/require"
+
+	helloworld "github.com/strongishllama/golang-action/internal/hello-world"
 )
 
 func TestRun(t *testing.T) {
-	// // ...
-	// actionLog := bytes.NewBuffer(nil)
-	// envMap := map[string]string{
-	// 	"INPUT_EXAMOKE_INPUT":  "user",
-	// 	"INPUT_LEASE-DURATION": "1h",
-	// }
-	// getenv := func(key string) string {
-	// 	return envMap[key]
-	// }
-	// action := githubactions.New(
-	// 	githubactions.WithWriter(actionLog),
-	// 	githubactions.WithGetenv(getenv),
-	// )
-	// // ...
-	// it.Equal("...", actionLog.String())
+	actionLog := bytes.NewBuffer(nil)
+	envMap := map[string]string{
+		"INPUT_EXAMPLE-INPUT": "hello-world",
+	}
+	getenv := func(key string) string {
+		return envMap[key]
+	}
+
+	cfg, err := helloworld.NewConfig(githubactions.New(
+		githubactions.WithWriter(actionLog),
+		githubactions.WithGetenv(getenv),
+	))
+	require.NoError(t, err)
+
+	require.NoError(t, helloworld.Run(context.Background(), cfg))
+	require.Equal(t, "Example input: hello-world\n", actionLog.String())
 }
