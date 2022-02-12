b9c08b6b0852a638e348850be04d3e2b826cb860
Taliesin Millhouse <taliesin.millhouse@maxkelsen.com>
2022-02-12 15:39:32 +1100

    feat: better logging

--- internal/hello-world/run.go
+++ internal/hello-world/run.go
@@ -6,8 +6,5 @@ import (
 
 func Run(ctx context.Context, cfg config) error {
 	cfg.action.Infof("Example input: %s", cfg.ExampleInput)
-	cfg.action.Infof("Example info")
-	cfg.action.Errorf("Example error")
-	cfg.action.Fatalf("Example fatal")
 	return nil
 }
