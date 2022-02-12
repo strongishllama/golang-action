a76ef19afc7100beff00e79a1cd3565a1af3b2f0
Taliesin Millhouse <taliesin.millhouse@maxkelsen.com>
2022-02-12 14:58:50 +1100

    fix: more binary logging

--- internal/hello-world/run.go
+++ internal/hello-world/run.go
@@ -5,7 +5,9 @@ import (
 )
 
 func Run(ctx context.Context, cfg config) error {
-	cfg.action.Debugf("Hello world!")
-	cfg.action.Debugf(cfg.ExampleInput)
+	cfg.action.Debugf("Example input: %s", cfg.ExampleInput)
+	cfg.action.Debugf("Example debug")
+	cfg.action.Errorf("Example error")
+	cfg.action.Fatalf("Example fatal")
 	return nil
 }
