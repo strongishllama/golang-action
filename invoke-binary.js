0320da4cd46c5726d9bf1c83fc988e34a688ff9b
Taliesin Millhouse <taliesin.millhouse@maxkelsen.com>
2022-02-12 15:22:22 +1100

    fix: binary path issue

--- invoke-binary.js
+++ invoke-binary.js
@@ -1,7 +1,14 @@
 const process = require("process");
 const childProcess = require("child_process");
 
-const binaryPath = `${__dirname}/bin/golang-action-${process.platform}-${process.arch}`;
+let binaryPath = `${__dirname}/bin/golang-action-${process.platform}-`;
+
+if (process.platform === "linux" && process.arch === "x64") {
+  binaryPath += "amd64";
+} else {
+  console.error(`Unsupported platform: ${process.platform} or architecture: ${process.arch}`);
+}
+
 console.log(`Executing binary at: ${binaryPath}`);
 
 const spawnSyncReturns = childProcess.spawnSync(binaryPath, { stdio: "inherit" });
